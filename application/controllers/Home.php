524b65f6ed3db6cb49dff91fb0b6cc72ff4792fd
Kylermugs <kylermugot@gmail.com>
2017-02-17 23:30:49 +0800

    Update Home.php
    main and employer controller method

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -22,18 +22,25 @@ class Home extends CI_Controller {
 		$this->load->view('partials/footer');
 	}
 
-
-	public function login()
-	{	
-		$this->load->view('partials/header');
-		$this->load->view('home/login');
-		$this->load->view('partials/footer');
-	}
-	
 	public function contact()
 	{	
 		$this->load->view('partials/header');
 		$this->load->view('home/contact');
 		$this->load->view('partials/footer');
 	}
+	
+	public function employer()
+	{	
+		$this->load->view('partials/header');
+		$this->load->view('home/employer');
+		$this->load->view('partials/footer');
+	}
+
+	public function main()
+	{	
+		$this->load->view('partials/header');
+		$this->load->view('home/main');
+		$this->load->view('partials/footer');
+	}
+	
 }
