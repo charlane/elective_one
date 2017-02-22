2e9757a3511ddff993f827b92b3ee10d66820cfe
gjsibugxvi <gjsibug8@gmail.com>
2017-02-23 00:55:12 +0800

    Add files via upload
    Employer profile.

---
+++ application/views/home/employerProfile.php
@@ -0,0 +1,125 @@
+<div class= "container-fluid"><!-- Fly-in navbar -->
+<div class="navbar navbar-default navbar-fixed-top" data-spy="affix" data-offset-top="80" id="nav">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="col-md-3">
+                <a class="navbar-brand" rel="home" href="#">
+                    <img style="max-width:200px; margin-top: -40px;"
+                    src="<?php echo base_url('resources/images/logo.png');?>">
+                </a>
+
+            </div>
+        </div>
+        <div class=""></div>
+      
+
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav borderbot right">
+
+                <li class="">
+                    <a href="<?php echo base_url('home/employer');?>">
+                        Job Posted</span>
+                    </a>
+                </li>
+
+                <li class="">
+                    <a href="<?php echo base_url('home/inbox');?>">
+                        Application Inbox  &nbsp;<span class="badge"> 5</span>
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>   
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <img style="max-width: 22px;"
+                            src="<?php echo base_url('resources/images/user.png');?>">
+
+                            <strong>&nbsp;Kyle&nbsp;</strong>
+                            <span class="glyphicon glyphicon-chevron-down"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <div class="navbar-login">
+                                    <div class="row">
+                                        <div class="">
+                                            <center>
+                                                <img style="max-width:100px;"
+                                                src="<?php echo base_url('resources/images/user.png');?>">
+                                            </center>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <h4 style="
+                                            text-align: center;
+                                            ">Hi!&nbsp;<strong>Kyle</strong></h4> 
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="divider navbar-login-session-bg"></li>
+                            <li><a href="<?php echo base_url('home/employerProfile');?>">My Profile <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Sign Out <span class="glyphicon glyphicon-log-out pull-right"></span></a></li>
+                            <li class="divider"></li>
+                        </ul>
+                    </li>
+                </ul>
+                
+            </ul>
+
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div><!--/.container -->
+            </div><!--/.navbar -->
+            <!-- nav end -->
+
+	<div class="outer"></div>
+	<br />
+		<div class= "container">
+			<div class= "col-md-12 margtopcontainer well">
+				<div class="col-md-3"><center><br>
+					<img width="100%" src="<?php echo base_url('resources/images/user.png');?>">
+					<h3>GJ Flowershop</h3>
+					</center>
+					<hr>
+					
+						<h4><a><span class="glyphicon glyphicon-user"></span></a> &nbsp; gjsibugxvi</h4>
+						<h4><a><span class="glyphicon glyphicon-envelope"></span></a> &nbsp; gjsibug@gmail.com</h4>
+						<h4><a><span class="glyphicon glyphicon-phone"></span></a> &nbsp; 09260021116</h4>
+					<br>
+					
+				</div>
+
+				<div class="col-md-8">
+					<h2 style="margin: 25px">Company Description</h2>
+					<p style="margin: 25px">NexusBond Asia Inc is a digital marketing company that provides a wide range of online solutions such as web development, graphic design, SEO, and more. We are currently looking for skillful applicants to become part of our CDO team. <br><br>
+
+              We are in need of MOBILE APP DEVELOPERS (for both Android and IOS) with the following qualifications:
+
+              Able to read documentation
+              Has full understanding of the basics of databases and relational data as well as the basics of JSON
+              For IOS, applicants must: <br><br>
+
+              Have an experience in Objective C or Swift<br>
+              Be excellent in UI integration<br>
+              Have a relevant experience in objective oriented programming<br>
+              Have at least 2 years of experience in iPhone/iPad development<br>
+              Be knowledgeable in iPhone 6.0+SDK, Cocoa-Touch, XCode IDE orientation<br>
+              Possess ample knowledge in various web services such as XML and JSON<br>
+              Have strong user interface design skills<br><br>
+             
+				</div>
+				<button class="btn btn-default"><span class="glyphicon glyphicon-edit"></span> &nbsp;Edit Profile</button><hr>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="clearfix"></div>
+
